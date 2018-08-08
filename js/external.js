764d9eb2fa3a4c403a690fac6d68046067c9903a
MichaelWaymire <mike.waymire2@gmail.com>
2018-08-08 11:30:42 -0500

    added movie exercise

--- js/external.js
+++ js/external.js
@@ -1,10 +1,41 @@
 "use strict";
 
-var mage = ""
-var age = 32;
-var bday = "Happy Birthday! ";
-var your = "You are ";
-var day = " years old today!";
-mage = bday + your + age++ + day;
+alert("Welcome to my webpage!");
 
-alert(mage);
+// promopt for colore + aleart w/response
+
+var color = prompt('What is your favorite color?');
+console.log('What is your favorite color?' + color);
+
+
+alert("Great, " + color + " is my favorite too!");
+
+// Step 3
+
+// You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear
+// (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it).
+// If price for a movie per day is $3, how much will you have to pay?
+var theLittleMermaid = 3;
+var brotherBear = 5;
+var hercules = 1;
+var tdaily = 3;
+var mtotal = (theLittleMermaid + brotherBear + hercules) * 3;
+
+alert("I rented 3 movies; The Little Mermaid for " + theLittleMermaid + " days, Brother Bear for " +
+    brotherBear + " days, and Hercules for " + hercules + " day." + " The total came out to $" + mtotal);
+
+
+
+
+
+// Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook,
+// they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350.
+// How much will you receive in payment for this week? You worked 10 hours for Facebook, 6
+// hours for Google and 4 hours for Amazon.
+
+// A student can be enrolled to a class only if the class is not full and the class
+// schedule does not conflict with her current schedule.
+
+
+// A product offer can be applied only if people buys more than 2 items, and the
+// offer has not expired. Premium members do not need to buy a specific amount of products.
