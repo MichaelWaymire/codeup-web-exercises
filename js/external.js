b5e532bf0b669860f90268c05635db446104905b
MichaelWaymire <mike.waymire2@gmail.com>
2018-08-08 10:19:58 -0500

    created js folder / external/inline.html and external.j

---
+++ js/external.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var mage = ""
+var age = 32;
+var bday = "Happy Birthday! ";
+var your = "You are ";
+var day = " years old today!";
+mage = bday + your + age++ + day;
+
+alert(mage);
