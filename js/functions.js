16945ef4ea27ca370cd110d210c9ff59f6ea7097
MichaelWaymire <mike.waymire2@gmail.com>
2018-08-09 11:38:31 -0500

    Finished funcations created conditiaons html/js

--- js/functions.js
+++ js/functions.js
@@ -8,12 +8,12 @@
 // //  * Example
 // //  * > sayHello("codeup") // returns "Hello, codeup!"
 // //  */
-//
-// var name = prompt("what is your name");
-// function sayHello(name){
-//    return ("Hello, " + name);
-// }
-// console.log(sayHello(name));
+
+var name = prompt("what is your name");
+function sayHello(name){
+   return ("Hello, " + name);
+}
+console.log(sayHello(name));
 //
 // // /**
 // //  * TODO:
@@ -22,10 +22,10 @@
 // //  *
 // //  * console.log 'helloMessage' to check your work
 // //  */
-// var helloMessage = sayHello("Mike")
-//
-//
-// console.log(helloMessage);
+var helloMessage = sayHello("Mike")
+
+
+console.log(helloMessage);
 //
 // // /**
 // //  * TODO:
@@ -33,79 +33,98 @@
 // //  * variable to the 'sayHello' function. You should see the same output in the
 // //  * console.
 // //  */
-//
-// var myName = sayHello("Mike");
-//
-// console.log(myName);
+
+var myName = sayHello("Mike");
+
+console.log(myName);
 
 
 // Don't modify the following line, it generates a random number between 1 and 3
 // and stores it in a variable named random
 var random = Math.floor((Math.random() * 3) + 1);
 
-// /**
-//  * TODO:
-//  * Create a function called 'isTwo' that takes a number as a parameter.
-//  * The function should return a boolean value based on whether or not the passed
-//  * number is the number 2.
-//  *
-//  * Example
-//  * > isTwo(1) // returns false
-//  * > isTwo(2) // returns true
-//  * > isTwo(3) // returns false
-//  *
-//  * Call the function 'isTwo' passing the variable 'random' as a argument.
-//  *
-//  * console.log *outside of the function* to check your work (you should see a
-//  * different result everytime you refresh the page if you are using the random
-//  * number)
-//  */
+/**
+ * TODO:
+ * Create a function called 'isTwo' that takes a number as a parameter.
+ * The function should return a boolean value based on whether or not the passed
+ * number is the number 2.
+ *
+ * Example
+ * > isTwo(1) // returns false
+ * > isTwo(2) // returns true
+ * > isTwo(3) // returns false
+ *
+ * Call the function 'isTwo' passing the variable 'random' as a argument.
+ *
+ * console.log *outside of the function* to check your work (you should see a
+ * different result everytime you refresh the page if you are using the random
+ * number)
+ */
 
 
-function isTwo(random) {
+function isTwo(input) {
 
-    var ranumgenerator1 = random >= 2;
-    var rannumgenrator2 = random >= 2;
-    var rannumgenrator3 = random <= 2;
-    return (random && rannumgenrator2 || ranumgenerator1 || rannumgenrator3);
+    return input === 2
 
 
 }
-console.log(isTwo());
+console.log(isTwo(random));
+console.log("The number was " + random);
 
 
-// /**
-//  * TODO:
-//  * Create a function named 'calculateTip' to calculate a tip on a bill at a
-//  * restaurant. The function should accept a tip percentage and the total of the
-//  * bill, and return the amount to tip
-//  *
-//  * Examples:
-//  * > calculateTip(0.20, 20) // returns 4
-//  * > calculateTip(0.25, 25.50) // returns 6.37
-//  * > calculateTip(0.15, 33.42) // returns 5.013
-//  */
+/**
+ * TODO:
+ * Create a function named 'calculateTip' to calculate a tip on a bill at a
+ * restaurant. The function should accept a tip percentage and the total of the
+ * bill, and return the amount to tip
+ *
+ * Examples:
+ * > calculateTip(0.20, 20) // returns 4
+ * > calculateTip(0.25, 25.50) // returns 6.37
+ * > calculateTip(0.15, 33.42) // returns 5.013
+ */
+var bill = prompt("What was your bill?")
+var tipRate = prompt("How much do you want to Tip (example 10% = .10)");
+
+function calculateTip(bill, tipRate){
+
+    return bill * tipRate;
 
 
-//
-// /**
-//  * TODO:
-//  * Use prompt and alert in combination with your calculateTip function to
-//  * prompt the user for the bill total and a percentage they would like to tip,
-//  * then display the dollar amount they should tip
-//  */
-//
-// /**
-//  * TODO:
-//  * Create a function named `applyDiscount`. This function should accept a price
-//  * (before a discount is applied), and a discount percentage (a number between 0
-//  * and 1). It should return the result of applying the discount to the original
-//  * price.
-//  *
-//  * Example:
-//  * > var originalPrice = 100;
-//  * > var dicountPercent = .2; // 20%
-//  * > applyDiscount(originalPrice, dicountPercent) // 80
-//  *
-//  * > applyDiscount(45.99, 0.12) // 40.4712
-//  */
+}
+var tip = calculateTip(bill, tipRate);
+console.log("The Tip is " + tip);
+ alert("This is how much you should tip: $" + tip);
+
+/**
+ * TODO:
+ * Use prompt and alert in combination with your calculateTip function to
+ * prompt the user for the bill total and a percentage they would like to tip,
+ * then display the dollar amount they should tip
+ */
+
+/**
+ * TODO:
+ * Create a function named `applyDiscount`. This function should accept a price
+ * (before a discount is applied), and a discount percentage (a number between 0
+ * and 1). It should return the result of applying the discount to the original
+ * price.
+ *
+ * Example:
+ * > var originalPrice = 100;
+ * > var dicountPercent = .2; // 20%
+ * > applyDiscount(originalPrice, dicountPercent) // 80
+ *
+ * > applyDiscount(45.99, 0.12) // 40.4712
+ */
+
+var price = prompt("What was the Total?");
+var discount = prompt("what was the Discount (Example: 20% = .2)");
+
+function applyDiscount(price, discount) {
+    return price * discount;
+
+}
+var total = applyDiscount(price, discount);
+console.log("You will save $" + total);
+alert("You will save $" + total);
--- js/js_test.js
+++ js/js_test.js
@@ -1,15 +1,50 @@
-// function makeAwesome() {
-//     alert("You are awesome")
-// }
-//
-// makeAwesome()
+//Conditionals examples
+function add10(number){
+    return console.log(number + 10)
 
-function alertMessage(message) {
-    alert(message);
 }
 
-alertMessage("Hello");
-alertMessage()
+console.log(add10(10))
 
+
+if (weather === "raining") {
+    bring(umbrella)
+} else {
+    bring("sunGlasses")
+}
+
+if (servays === 50) {
+    alert("tacos!!!");
+} else {
+    alert("NO TACOS")
+}
+
+
+if (caffeine > 0) {
+    awake = true
+} else {
+    drinkCoffee("stat");
+}
+
+if (batter > .2) {
+    turnOffWifiP();
+    turnOffGPS();
+    turnOffBluetooth();
+
+}
+
+var favoriteCar = prompt("What is your favorite car Brand?");
+
+if (favoriteCar === "Ferarri") {
+    alert("WoW thats pricy")
+}
+else if (favoriteCar === "Tesla") {
+    alert("Enviromental!")
+} else {
+
+}
+
+
+//Ternary Operator
 
 
