9dcaf97f23db826aca597e1e660aeffaa5c76d22
MichaelWaymire <mike.waymire2@gmail.com>
2018-08-29 16:20:11 -0500

    touch ups

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -20,3 +20,45 @@ $("li").click(function () {
 });
 
 
+var konamieCode = {
+
+	konami: function () {
+		var konamikeys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13],
+			started = false,
+			count = 0;
+
+		$(document).keydown(function (e) {
+			var reset = function () {
+				started = false;
+				count = 0;
+				return;
+			};
+
+			var key = e.keyCode;
+			if (!started) {
+				if (key == 38) {
+					started = true;
+				}
+			}
+			if (started) {
+				if (konamikeys[count] == key) {
+					count++;
+				} else {
+					// Incorrect key, restart.
+					reset();
+				}
+				if (count == 11) {
+					// Success!
+					onlyYou.play();
+
+
+					reset();
+				}
+			} else {
+				reset();
+			}
+		});
+	}
+};
+
+konamieCode.konami();
