e912814a26d8f03cdd7a3460d9916ca629235d1f
MichaelWaymire <mike.waymire2@gmail.com>
2018-08-29 15:03:57 -0500

    Finished exercise travers

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
-	$("button").click(function () {
+	$("#showHideFacts").click(function () {
 		$("dd").toggleClass("invisible");
 	});
 
+});
+
+
+$("#liBackgroundColor").click(function () {
+	$("li").css('background-color', '#8f261e');
+});
+
+
+$("h3").click(function () {
+	$("li").css('font-weight', 'bold');
+});
+
+$("li").click(function () {
+	$(this).parent().children().first().css('color', 'blue');
 });
 
 
