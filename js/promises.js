3eb566fce233a21373c6ee4c82268d6122e9430b
MichaelWaymire <mike.waymire2@gmail.com>
2018-09-11 09:15:19 -0500

    finished promise exersices

--- js/promises.js
+++ js/promises.js
@@ -1,28 +1,27 @@
 
 
 
-const wait = new Promise((resolve, reject) => {
-	if (Math.random() > 0.5) {
-		resolve('You\'ll see this after 1 second');
-	} else {
-		reject('You\'ll see this after 3 seconds');
-	}
-});
+const isSmallThenTen = (num) => {
+	return new Promise((resolve, reject) => {
+		if(num < 10) {
+			resolve("Its less then 10");
+			console.log(resolve)
+		} else {
+			reject("Its more then 10");
+			console.log(reject)
+		}
+	})
+};
+
+console.log(isSmallThenTen(1));
 
 
-
-// const wait = (num) => {
-// 	return new Promise((resolve, reject) => {
-// 		if(num < 10) {
-// 			resolve(true)
-// 		} else {
-// 			reject(false)
-// 		}
-// 	})
-// }'
-
-
-
-// fetch(url, {headers: {'Authorization': 'token input token here'}})
+function getLastCommit (){
+	return fetch('https://api.github.com/users/michaelwaymire/events',{headers:{'Authorization':'aa44610c8836c557f06c9f1a2376c4bca4a1d82'}})
+		.then(response => response.json())
+		.then((users) => (users[0].created_at))
+		.catch(error => console.log(error))
+}
+getLastCommit().then(data => data).then(Date => console.log(Date));
 
 
